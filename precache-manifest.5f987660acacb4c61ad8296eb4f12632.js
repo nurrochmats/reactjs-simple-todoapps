711393472d3f7894a71dfd0be74a8b17813009c7
Nurrochmat Saptoaji <nurrochmat.sapto@gmail.com>
2020-07-20 22:39:09 +0700

    Updates

---
+++ precache-manifest.5f987660acacb4c61ad8296eb4f12632.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ade79ba0aaae425c1e7da5711310b3fb",
+    "url": "/simple-todoapps-with-reactjs/index.html"
+  },
+  {
+    "revision": "98a84502465f8fbb822f",
+    "url": "/simple-todoapps-with-reactjs/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4fe9053fb52338eb2a86",
+    "url": "/simple-todoapps-with-reactjs/static/js/2.2f255bd8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todoapps-with-reactjs/static/js/2.2f255bd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98a84502465f8fbb822f",
+    "url": "/simple-todoapps-with-reactjs/static/js/main.261c94e3.chunk.js"
+  },
+  {
+    "revision": "08ba0979169da944d8a2",
+    "url": "/simple-todoapps-with-reactjs/static/js/runtime-main.c94c9fb2.js"
+  }
+]);
